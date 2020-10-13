09dc3fbb67ecaa06ead8868ce03e04c2ee34ac27
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-10-13 14:49:51 +0300

    Add helper scripts for manual deployments

--- deploy-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-ORG=${ORG:-hsldevcom}
-PROJECT_NAME=${PROJECT_NAME:-hsl-jore-postgis}
-DOCKER_TAG=${ORG}/${PROJECT_NAME}:latest
-
-docker build --build-arg INCLUDE_JORE_STATIC=true -t $DOCKER_TAG .
-docker push $DOCKER_TAG
